04246ba9a9aa950cd86b2e0400de6b349ec6b0e7
Andika Ahmad Ramadhan <andikahmadr@gmail.com>
2023-01-30 23:35:01 +0700

    chore: remove finished TODO comments

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
 	"config-syncer/cmd"
 )
 
-// TODO: wrap in cobra style
-// TODO: implement sync using configurable YAML config file instead of using src-secret and dest-secret
 // TODO: prepare helm charts
 
 func main() {
